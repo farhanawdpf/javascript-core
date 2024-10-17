f75b79cc386cbd25d9fb80fd28c6cdfda8cb6f94
farhanawdpf <farhanawdpf@gmail.com>
2024-10-17 12:52:54 +0600

    update

---
+++ data-type-variable/main.js
@@ -0,0 +1,20 @@
+document.write("variable");
+document.write("<br>");
+var a=3;
+var a=4;
+var a=5;
+document.write(a);
+document.write("<br>");
+document.write("const");
+document.write("<br>");
+const b=6;
+document.write(b);
+
+document.write("<br>");
+document.write("Let");
+
+document.write("<br>");
+let c=4;
+c=5;
+c=6;
+document.write(c);
